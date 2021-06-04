3cef32796929119e081b2ed20358bfaf8c728e2c
James Moseley <jamesmoseley@jamess-mbp.lan>
2021-06-03 18:56:48 -0700

    can not accept text type

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("connected on port 3000"));
 
+// for get requests when using as api
 app.get('/', (req, res) => {
-    const urlLink = "https://en.wikipedia.org/wiki/Frances_Gertrude_McGill";
+    const urlLink = "https://en.wikipedia.org/wiki/Feast_of_Corpus_Christi";
 
     axios.get(urlLink).then(urlResponse => {
             let config = {
@@ -36,12 +37,13 @@ app.get('/', (req, res) => {
         }, (error) => console.log("server side err"));
 })
 
-
-
+// post requests for using with my front end.  
 app.post('/', (req, res) => {
     let urlLink = req.body.url_box;
     let api = req.body.api_box;
-    console.log("server linke ", urlLink);
+    let text_type = req.body.text_type;
+
+    console.log("server is linked to url:", urlLink);
     // res.send(urlLink);
 
     axios.get(urlLink).then(urlResponse => {
@@ -52,18 +54,24 @@ app.post('/', (req, res) => {
                 }
             const $ = cheerio.load(urlResponse.data);
             $('body').each((i, element) => {
+                const head = $(element) 
+                .find("h1")
+                .text();
+    
                 const bodyText = $(element)
                 .find("p")
                 .text();
                 
-                console.log(api);
+                if(text_type==="p")
+                    axios.post(api, bodyText, config)
+                    .then(translated_text => res.send(translated_text.data))
+                        .catch(error=> console.log(error));
+                else 
+                    axios.post(api, head, config)
+                    .then(translated_text => res.send(translated_text.data))
+                        .catch(error=> console.log(error));
 
-                axios.post(api, bodyText, config)
-                .then(translated_text => res.send(translated_text.data))
-                    .catch(error=> console.log(error));
                 
-            
-
             })
         }, (error) => console.log("server side err"));
 
